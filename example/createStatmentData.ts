334706a17840b6053db53db03b89aed65a148110
Ed Sungik Choi <sungik.dev@gmail.com>
2021-01-23 19:51:04 +0900

    createStatementData 함수 파일로 분리

---
+++ example/createStatmentData.ts
@@ -0,0 +1,61 @@
+import { Invoice, Plays, Performance, StatementData, PerformanceStatement } from "./types"
+
+function createStatementData(invoice: Invoice, plays: Plays) {
+  const statementData = <StatementData>{};
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+  return statementData;
+
+  function enrichPerformance(aPerformance: Performance) {
+    const result = <PerformanceStatement>Object.assign({}, aPerformance);
+    result.play = playFor(result);
+    result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
+    return result;
+  }
+
+  function playFor(aPerformance: Performance) {
+    return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance: PerformanceStatement) {
+    let result = 0;
+    switch (aPerformance.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
+    }
+    return result;
+  }
+
+  function volumeCreditsFor(aPerformance: PerformanceStatement) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
+
+  function totalVolumeCredits(data: StatementData) {
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+  }
+
+  function totalAmount(data: StatementData) {
+    return data.performances.reduce((total, p) => total + p.amount, 0);
+  }
+}
+
+export default createStatementData
--- example/types.ts
+++ example/types.ts
@@ -1,20 +1,32 @@
-export type PlayType = "hamlet" | "as-like" | "othello"
+export type PlayType = "hamlet" | "as-like" | "othello";
 
 export type Performance = {
-  playID: PlayType
-  audience: number
-}
+  playID: PlayType;
+  audience: number;
+};
 
 export interface Play {
-  name: string
-  type: "tragedy" | "comedy"
+  name: string;
+  type: "tragedy" | "comedy";
 }
 
 export type Plays = {
-  [key in PlayType]: Play
-}
+  [key in PlayType]: Play;
+};
 
 export interface Invoice {
-  customer: string,
-  performances: Performance[]
+  customer: string;
+  performances: Performance[];
+}
+
+export interface PerformanceStatement extends Performance {
+  play: Play;
+  amount: number;
+  volumeCredits: number;
+}
+
+export interface StatementData extends Invoice {
+  performances: PerformanceStatement[];
+  totalAmount: number;
+  totalVolumeCredits: number;
 }
