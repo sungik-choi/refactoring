984edfe577d726e3aba33fabf578664929b64543
Ed Sungik Choi <sungik.dev@gmail.com>
2021-01-23 19:38:21 +0900

    반복문을 파이프라인으로 바꾸기

--- example/index.ts
+++ example/index.ts
@@ -64,19 +64,11 @@ function statement(invoice: Invoice, plays: Plays) {
   }
 
   function totalVolumeCredits(data: StatementData) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 
   function totalAmount(data: StatementData) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.amount, 0);
   }
 }
 
