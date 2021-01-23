4ce450d6950a6301bf1d13d0c1fd162a716697a4
Ed Sungik Choi <sungik.dev@gmail.com>
2021-01-23 19:59:32 +0900

    HTML 데이터 생성 함수 작성

--- example/statement.ts
+++ example/statement.ts
@@ -2,7 +2,7 @@ import invoicesData from "./data/invoices";
 import playsData from "./data/plays";
 
 import { Invoice, Plays, StatementData } from "./types";
-import createStatementData from './createStatmentData';
+import createStatementData from "./createStatmentData";
 
 function statement(invoice: Invoice, plays: Plays) {
   return renderPlainText(createStatementData(invoice, plays));
@@ -26,6 +26,32 @@ function renderPlainText(data: StatementData) {
       minimumFractionDigits: 2,
     }).format(aNumber / 100);
   }
+}
+
+function htmlStatement(invoice: Invoice, plays: Plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data: StatementData) {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
+  result += "<table>\n";
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+  return result;
+}
+
+function usd(aNumber: number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
 }
 
 function main() {
