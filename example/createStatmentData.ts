9e3d50c9eaac4164f533054ceb695f27c3ba1615
Ed Sungik Choi <sungik.dev@gmail.com>
2021-01-23 20:44:34 +0900

    세부 로직을 서브 클래스로 이동

--- example/createStatmentData.ts
+++ example/createStatmentData.ts
@@ -10,10 +10,7 @@ class PerformanceCalculator {
   }
 
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
-    if ("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
-    return result;
+    return Math.max(this.performance.audience - 30, 0);
   }
 }
 
@@ -35,6 +32,10 @@ class ComedyCalculator extends PerformanceCalculator {
     }
     result += 300 * this.performance.audience;
     return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
 
