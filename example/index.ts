0e4931dd1d95d79533a2ea4a17c7a8b3ef7cb547
Ed Sungik Choi <sungik.dev@gmail.com>
2021-01-23 18:45:16 +0900

    변수명 변경

--- example/index.ts
+++ example/index.ts
@@ -28,11 +28,11 @@ function playFor(aPerformance: Performance) {
   return plays[aPerformance.playID];
 }
 
-function volumeCreditsFor(perf: Performance) {
-  let volumeCredits = 0;
-  volumeCredits += Math.max(perf.audience - 30, 0);
-  if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
-  return volumeCredits;
+function volumeCreditsFor(aPerformance: Performance) {
+  let result = 0;
+  result += Math.max(aPerformance.audience - 30, 0);
+  if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+  return result;
 }
 
 function statement(invoice: Invoice, plays: Plays) {
