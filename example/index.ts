5efe6a8134403381912497fba2ffe6241146dc7b
Ed Sungik Choi <sungik.dev@gmail.com>
2021-01-23 18:47:25 +0900

    함수 변수를 일반 함수로 변경

--- example/index.ts
+++ example/index.ts
@@ -35,15 +35,18 @@ function volumeCreditsFor(aPerformance: Performance) {
   return result;
 }
 
+function format(aNumber: number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber);
+}
+
 function statement(invoice: Invoice, plays: Plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf)
