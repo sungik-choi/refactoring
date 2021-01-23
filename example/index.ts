56bbc20089a20c8f871fb84efaa948b895e5a6a6
Ed Sungik Choi <sungik.dev@gmail.com>
2021-01-23 19:00:01 +0900

    포맷팅

--- example/index.ts
+++ example/index.ts
@@ -4,7 +4,7 @@ import playsData from "./data/plays";
 
 function statement(invoice: Invoice, plays: Plays) {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-  
+
   for (let perf of invoice.performances) {
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
   }
